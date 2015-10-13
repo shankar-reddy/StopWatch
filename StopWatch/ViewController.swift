717d5656fc42de11cf28a6376aedeacbb6df2a25
Sankara Telukutla <sankara.telukutla@gmail.com>
2015-10-12 23:12:58 -0700

    Stop Watch initial files

--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -10,6 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var timerLabel: UILabel!
+    
+    var time = 0
+    var timer = NSTimer()
+    
+    func runTimer(){
+        time++
+        timerLabel.text = "\(time)"
+        timerLabel.textColor = UIColor.blueColor()
+    }
+    
+    @IBAction func stopAction(sender: AnyObject) {
+        timer.invalidate()
+        time = 0
+    }
+    
+    @IBAction func playAction(sender: AnyObject) {
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("runTimer"),userInfo: nil, repeats: true)
+    
+    }
+    
+    @IBAction func refresh(sender: AnyObject) {
+        
+        timer.invalidate()
+        time = 0
+        timerLabel.text = "\(time)"
+        
+    }
+    
+    
+    @IBAction func pauseAction(sender: AnyObject) {
+        timer.invalidate()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +54,6 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
